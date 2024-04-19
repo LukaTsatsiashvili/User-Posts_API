5f3605c1deaf456ffcc0626e45ac0b77bff30f35
luka tsatsiashvili <luka.tsatsiashvili.1@btu.edu.ge>
2024-04-19 21:35:41 +0400

    Some refactoring

---
+++ RepositoryLayer/Migrations/20240419153751_Modifying Images and Users table.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RepositoryLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModifyingImagesandUserstable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "UserId",
+                table: "Images",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Images_UserId",
+                table: "Images",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Images_Users_UserId",
+                table: "Images",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Images_Users_UserId",
+                table: "Images");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Images_UserId",
+                table: "Images");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Images");
+        }
+    }
+}
